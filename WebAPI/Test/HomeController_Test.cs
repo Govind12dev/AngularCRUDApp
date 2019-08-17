c4d9e68e453ae7555cc7cd02784f185e0fecb01c
Govind <govindasamy.murugesan2@chubb.com>
2019-08-17 14:58:38 +0800

    Add project files.

---
+++ WebAPI/Test/HomeController_Test.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Controllers;
+
+namespace WebAPI.Test
+{
+    [TestFixture]
+    public class HomeController_Test
+    {
+        HomeController homeController = new HomeController();
+
+        [Test]
+        public void Index_Test()
+        {
+            var result = homeController.Index();
+            Assert.IsNotNull(result);
+        }
+    }
+}
